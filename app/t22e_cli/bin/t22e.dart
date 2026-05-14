3b0d868e6378487e2271ea404f6f417b250dbc33
Francesco Iapicca <francesco@yakforward.com>
2026-05-14 18:29:34 +0300

    remove app

--- app/t22e_cli/bin/t22e.dart
+++
@@ -1,4 +0,0 @@
-/// Entry point that prints the framework banner.
-void main(List<String> arguments) {
-  print('t22e - A terminal user interface framework');
-}
--- app/t22e_cli/lib/t22e_cli.dart
+++
@@ -1,2 +0,0 @@
-/// Public API: the Program runtime and FPS throttler.
-export 'src/program.dart' show Program, FpsThrottle;
