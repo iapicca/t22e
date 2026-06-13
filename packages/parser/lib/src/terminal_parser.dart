8127ca1b3c40613c5253477b7184e906c572e3ac
iapicca <francesco@yakforward.com>
2026-06-13 19:52:35 +0300

    raw mode refctor - phase 3 - refactor terminal_parser/advance

--- packages/parser/lib/src/terminal_parser.dart
+++ packages/parser/lib/src/terminal_parser.dart
@@ -21,14 +21,21 @@ class TerminalParser {
   });
 
   /// TODO I want to manually rework this!
-  List<Event> advance(List<int> bytes) {
-    final events = <Event>[];
-    for (final seq in _engine.advanceAll(bytes)) {
-      final event = _interpret(seq);
-      if (event != null) events.add(event);
-    }
-    return events;
-  }
+  // List<Event> advance(List<int> bytes) {
+  //   final events = <Event>[];
+  //   for (final seq in _engine.advanceAll(bytes)) {
+  //     final event = _interpret(seq);
+  //     if (event != null) events.add(event);
+  //   }
+  //   return events;
+  // }
+
+    /// TODO (wrote by hand) I want this style to be analyzed and added to code-standards.md
+    List<Event> advance(List<int> bytes) => [
+for (final seq in _engine.advanceAll(bytes)) ?_interpret(seq)
+    ];
+  
+  
 
   Event? _interpret(SequenceData seq) {
     return switch (seq) {
