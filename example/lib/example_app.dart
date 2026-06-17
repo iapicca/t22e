1f7b058115a7b31bdb54e2041ab928dd2c23b0c4
iapicca <francesco@yakforward.com>
2026-06-17 18:42:35 +0300

    example app - example_app_plan phase 5 & 6

---
+++ example/lib/example_app.dart
@@ -0,0 +1,7 @@
+export 'src/chat_message.dart';
+export 'src/chat_model.dart';
+export 'src/messages.dart';
+export 'src/chat_app.dart';
+export 'src/chat_app_provider.dart';
+export 'src/chat_bubble.dart';
+export 'src/chat_view.dart';
